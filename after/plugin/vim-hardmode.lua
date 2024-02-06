f19e462d9b5fd26e0c0f0a76fa81d685eccb5dcc
Bob Maertz <r.maertz@gmail.com>
2024-02-06 06:40:30 -0500

    Backing out changes for hardmode shortcuts

--- after/plugin/vim-hardmode.lua
+++ after/plugin/vim-hardmode.lua
@@ -1,4 +1,4 @@
-hardmode = require("hardmode")
+--hardmode = require("hardmode")
 
-vim.keymap.set("n", "<leader>h", function() hardmode.HardMode() end, { remap = false })
-vim.keymap.set("n", "<leader>H", function() hardmode.EasyMode() end, { remap = false })
+--vim.keymap.set("n", "<leader>h", function() hardmode.HardMode() end, { remap = false })
+--vim.keymap.set("n", "<leader>H", function() hardmode.EasyMode() end, { remap = false })
