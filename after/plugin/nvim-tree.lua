8d0a32ec06212fd080013df1bb9669468ad634b3
Bob Maertz <r.maertz@gmail.com>
2024-02-06 07:02:41 -0500

    Adding a QOL keymap for nvimtree

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -5,19 +5,9 @@ vim.g.loaded_netrwPlugin = 1
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
--- empty setup using defaults
-require("nvim-tree").setup()
 
--- OR setup with some options
---require("nvim-tree").setup({
---  sort_by = "case_sensitive",
- -- view = {
-  --  width = 30,
- -- },
-  --enderer = {
-   -- group_empty = true,
-  --},
-  --filters = {
-   -- dotfiles = true,
-  --},
---})
+-- Shortcut to toggle the NvimTree
+vim.keymap.set('n', '<leader>x', ':NvimTreeToggle<CR>', opts('Toggle'))
+
+-- empty setup using defaults
+require("nvim-tree").setup(); 
